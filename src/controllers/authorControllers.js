0cd63feb24038319ec7e2d53a296ca23be4082e8
Adityakumar5050 <ak8445ch@gmail.com>
2022-09-05 19:57:41 +0530

    update all  changes

--- src/controllers/authorControllers.js
+++ src/controllers/authorControllers.js
@@ -1,5 +1,6 @@
 const authorModels = require('../models/authorModels');
 const createAuthor= async function (req, res) {
+    console.log(req.route)
     let newAuthor = req.body
     let authordetails = await authorModels.create(newAuthor)
     res.send({data: authordetails})
