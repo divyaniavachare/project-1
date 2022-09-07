5fed6b77b2880127d256e5fd8ca41dd965c9e2d8
Adityakumar5050 <ak8445ch@gmail.com>
2022-09-07 22:27:43 +0530

    updatechanges

--- src/controllers/authorControllers.js
+++ src/controllers/authorControllers.js
@@ -1,24 +1,57 @@
-const authorModel = require('../models/authorModel');
+const authorModel = require("../models/authorModel");
+const jwt = require("jsonwebtoken");
 
-const createAuthor = async function(req, res) {
-  try { 
+//----------------------------------------------------------------------------------//
+// This is the first api to create an author in database with email validation.
 
-  let data = req.body;
-  let savedData = await authorModel.create(data)
-  res.status(201).send( {msg: savedData})
-  }catch(err){
-  res.status(500).send({msg: err.message})
+const createAuthor = async function (req, res) {
+  try {
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let checkEmail = regex.test(req.body.email);
+    if (checkEmail) {
+      let myAuthor = req.body;
+      let authorSaved = await authorModel.create(myAuthor);
+      res.status(200).send({status:true, data: authorSaved });
+    } else {
+      res.status(400).send({status:false, message: "Please provide a Valid Email Id"});
+    }
+  } catch (error) {
+    res.status(500).send({ status: false, msg: error.message });
   }
+};
+
+//----------------------------------------------------------------------------------//
+// This is the login api of phase 2 for the authentication.
+
+const doLogin = async function (req, res) {
+  try {
+    userEmail = req.body.email;
+    userPassword = req.body.password;
+    let user = await authorModel.findOne({
+      email: userEmail,
+      password: userPassword,
+      isDeleted: false,
+    });
+
+    if (user) {
+      let fname = user.fname;
+      let lname = user.lname;
+      let payload = { userId: user._id, email: user.email };
+      const generatedToken = jwt.sign(payload, "Radium Star");
+      res.status(200).send({
+        Message: fname + " " + lname + " you have logged in Succesfully",
+        YourId: user._id,
+        token: generatedToken,
+      });
+    } else {
+      res.status(400).send({ status: false, message: "Oops...Invalid credentials" });
+    }
+  } catch (error) {
+    res.status(500).send({ status: false, msg: error.message });
   }
+};
 
-  
-  
+//---------------------------------------------------------------------------------//
 
-
-  
-
-
-
-
-module.exports.createAuthor = createAuthor
-  // module.exports.createAuthor=createAuthor
+module.exports.createAuthor = createAuthor;
+module.exports.doLogin = doLogin;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const authControl= require("../controllers/authorControllers")
-const blogControl=require("../controllers/blogsController")
+const authorControllers = require('../controllers/authorControllers')
+const blogControllers = require('../controllers/blogControllers') 
+const appMiddleware = require('../middleWares/appMiddleware')
 
+//Project 1 - Phase - 1
+// router.post('/Authors',authorController.createAuthor);
+router.post('/Authors',authorControllers.createAuthor);
+// router.post('/createBlog',appMiddleware.getAuthorDetails,blogController.createBlog);
+router.post('/createBlog',appMiddleware.getAuthorDetails,blogControllers.createBlog)
+router.get('/getBlog',appMiddleware.getAuthorDetails, blogControllers.returnBlogsFiltered) //
+router.put('/getUpdate/:id',appMiddleware.getAuthorDetails, blogControllers.updateData);
+router.delete('/deleteUpdate/:id',appMiddleware.getAuthorDetails, blogControllers.deleteBlog);
+router.delete('/deleteUpdate2',appMiddleware.getAuthorDetails, blogControllers.deleteSpecific);
 
-router.post("/authors", authControl.createAuthor)
-
-router.post("/blogs", blogControl.createBlog)
-
-router.get("/blog", blogControl.getBlog)
-
-// router.put("/update/:blogId", blogControl.updateBlog)
-router.put("/update/:blogId",blogControl.updateBlog)
-
-router.delete("/deleteBlog/:blogId",blogControl.deleteBlog)
-
-router.delete("/deleteBlogByQuery",blogControl.deleteBlogByQuery)
-
+//Project 1 - Phase - 2
+router.post('/doLogin',authorControllers.doLogin)
 
 module.exports = router;
