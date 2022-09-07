63b09c6f09a9d0d175e3495627d53ff605cab839
Ankit gupta <1729ankit@gmail.com>
2022-09-07 17:49:20 +0530

    ankit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const authControl= require("../controllers/authorControllers")
-const blogControl=require("../controllers/blogsController")
+const authControl = require("../controllers/authorControllers")
+const blogControl = require("../controllers/blogsController")
+const { authenticate, authorise } = require("../middleware/auth")
 
 
 router.post("/authors", authControl.createAuthor)
 
 router.post("/blogs", blogControl.createBlog)
 
-router.get("/blog", blogControl.getBlog)
+router.get("/blog", authenticate, authorise, blogControl.getBlog)
 
 // router.put("/update/:blogId", blogControl.updateBlog)
-router.put("/update/:blogId",blogControl.updateBlog)
+router.put("/update/:blogId", authenticate, authorise, blogControl.updateBlog)
 
-router.delete("/deleteBlog/:blogId",blogControl.deleteBlog)
+router.delete("/deleteBlog/:blogId", authenticate, authorise, blogControl.deleteBlog)
 
-router.delete("/deleteBlogByQuery",blogControl.deleteBlogByQuery)
-
+router.delete("/deleteBlogByQuery", authenticate, authorise, blogControl.deleteBlogByQuery)
+router.post("/login", authControl.loginUser)
 
 module.exports = router;
