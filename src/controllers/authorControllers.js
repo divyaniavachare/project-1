578ae51b1ba12199582bf6134c99bc1e59a8b6ec
Adityakumar5050 <ak8445ch@gmail.com>
2022-09-08 12:17:52 +0530

    done

--- src/controllers/authorControllers.js
+++ src/controllers/authorControllers.js
@@ -1,31 +1,46 @@
-const authorModels = require("../models/authorModel");
+const authorModel = require("../models/authorModel");
 const jwt = require("jsonwebtoken");
 
 //----------------------------------------------------------------------------------//
 // This is the first api to create an author in database with email validation.
-const isValid = function (value) {
-  if (typeof value === 'undefined' || value === null) return false
-  if (typeof value === 'string' && value.trim().length === 0) return false
-  return true;
-}
-const isValidRequestBody = function (requestBody) {
-  return Object.keys(requestBody).length > 0
-}
-const isValidEmail=function(email){
-  return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
-}
-const regexValidator=function(value){
-  let regex= /^[a-zA-Z]+([\s][a-zA-Z]+)*$/
-  return regex.test(value)
-}
+
 const createAuthor = async function (req, res) {
-    try {
-        let data = req.body
-        let savedData = await authorModel.create(data)
-        res.status(201).send({ msg: savedData })
-    } catch (error) {
-        res.status(500).send({ status: false, message: error.message })
-    }
+  try {
+      let data = req.body;
+      if(!isValidRequestBody(data)){
+          return res.status(400).send({status:false,msg:"invalid"})
+        }
+          if (!data)return res.status(400).send({status:false,msg:"enter data"})
+      
+      let { fname, lname, title, email, password } = data
+      if (!fname) { return res.status(400).send({ status: false, msg: "fname is required" }) }
+      if (!lname) { return res.status(400).send({ status: false, msg: "lname is required" }) }
+      if (!title) { return res.status(400).send({ status: false, msg: "title is required" }) }
+      if (!email) { return res.status(400).send({ status: false, msg: "email is required" }) }
+      if (!password) { return res.status(400).send({ status: false, msg: "password is required" }) }
+
+      if (typeof fname !== "string" || fname[0] == " " || fname[fname.length - 1] == " ") {
+          return res.status(400).send({ status: false, msg: "please enter valid fname" })
+      }
+
+      if (typeof lname !== "string" || lname[0] == " " || lname[lname.length - 1] == " ") {
+          return res.status(400).send({ status: false, msg: "please enter valid lname" })
+      }
+
+      if (title !== "Mr" && title !== "Mrs" && title !== "Miss") {
+          return res.status(400).send({ status: false, msg: "please enter valid title" })
+      }
+
+      let findEmail = await authorModel.findOne({ email: email })
+      if (findEmail) {
+          return res.status(400).send({ status: false, msg: "email already exist" })
+      }
+      fname.trim()
+      let savedData = await authorModel.create(data)
+      res.status(201).send({ msg: savedData })
+  } catch (err) {
+      res.status(500).send({ msg: err.message })
+  }
 }
 
 //----------------------------------------------------------------------------------//
@@ -35,7 +50,7 @@ const doLogin = async function (req, res) {
   try {
     userEmail = req.body.email;
     userPassword = req.body.password;
-    let user = await authorModels.findOne({
+    let user = await authorModel.findOne({
       email: userEmail,
       password: userPassword,
       isDeleted: false,
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,17 +7,15 @@ const appMiddleware = require('../middleWares/appMiddleware')
 
 
 //Project 1 - Phase - 1
-// router.post('/Authors',authorController.createAuthor);
-router.post('/Authors',appMiddleware.authorization,getAuthorDetails,authorControllers.createAuthor);
-// router.post('/createBlog',appMiddleware.getAuthorDetails,blogController.createBlog);
-router.post('/createBlog',appMiddleware.authorization,getAuthorDetails,blogControllers.createBlog)
-router.get('/getBlog',appMiddleware.getAuthorDetails,authorization, blogControllers.returnBlogsFiltered) //
-router.put('/getUpdate/:id',appMiddleware.getAuthorDet,ailsauthorization, blogControllers.updateData);
-router.delete('/deleteUpdate/:id',appMiddleware.getAuthorDetails,authorization, blogControllers.deleteBlog);
-router.delete('/deleteUpdate2',appMiddleware.getAuthorDetails,authorization, blogControllers.deleteSpecific);
+router.post('/Authors',authorControllers.createAuthor);
+router.post('/createBlog',appMiddleware.getAuthorDetails,appMiddleware.authorization,blogControllers.createBlog)
+router.get('/getBlog',appMiddleware.getAuthorDetails,appMiddleware.authorization, blogControllers.returnBlogsFiltered) //
+router.put('/getUpdate/:id',appMiddleware.getAuthorDetails ,appMiddleware.authorization, blogControllers.updateData);
+router.delete('/deleteUpdate/:id',appMiddleware.getAuthorDetails,appMiddleware.authorization, blogControllers.deleteBlog);
+router.delete('/deleteUpdate2',appMiddleware.getAuthorDetails,appMiddleware.authorization, blogControllers.deleteSpecific);
 
 //Project 1 - Phase - 2
-router.post('/doLogin',appMiddleware.authorization,getAuthorDetails,authorControllers.doLogin)
+router.post('/doLogin',appMiddleware.getAuthorDetails,appMiddleware.authorization,authorControllers.doLogin)
 
 
 module.exports = router;
