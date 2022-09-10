56e9f8881fe5f0c351a11244f5f4efd3efd2dcb5
Adityakumar5050 <ak8445ch@gmail.com>
2022-09-10 22:49:07 +0530

    done

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -1,62 +1,54 @@
-const jwt = require('jsonwebtoken')
-const blogModel = require('../model/blogModel')
-const mongoose=require('mongoose')
+const jwt = require("jsonwebtoken");
 
 
 
-//-----------------------------------------------------------------------------//
-// This is the middleware will be used for the authentication purpose
 
-const getAuthorDetails = async function (req, res, next) {
+//*********************************************AUTHENTICATION************************************************************************
+
+const authentication = function (req, res, next) {
     try {
-        let token = req.headers['x-api-key']
-        let decodedToken = jwt.verify(token, "plutonium Star")
-        if (decodedToken) {
-            req.userId = decodedToken.userId
-            next();
-        } else {
-            res.send({ status: false, message: 'Token not valid' })
+        let token = req.headers["x-api-key"];
+        if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
+        let decodedToken = jwt.verify(token, "functionUp-plutonium-project-key")
+        //req.decodedToken = decodedToken
+        if (!decodedToken) {
+            return res.status(403).send({ status: false, msg: "token is invalid" });
         }
-    } catch (error) {
-        res.status(500).send({ status: false, msg: error.message })
-    }
-}
-//-----------------------------------------------------------------------------//
-const authorization = async function (req, res, next) {
-    try {
-        let token = req.headers['x-api-key']
-        let ObjectID = mongoose.Types.ObjectId
-        let decodedToken = jwt.verify(token, "tokensecretKey")
-        if (req.query.authorId) {
-            let authorId = req.query.authorId
-            if (!ObjectID.isValid(authorId)) { return res.status(400).send({ status: false, message: "Not a valid AuthorID" }) }
-            if (authorId != decodedToken.authorId) {
-                return res.status(403).send({ status: false, message: "You are not a authorized user" })
+        let bodyAuthorId = req.body.authorId;
+        if (bodyAuthorId) {
+            if (bodyAuthorId != decodedToken.authorId)
+                return res.status(400).send({
+                    status: false,
+                    message: "Provided authorId is not same as logined auhorId",
+                });
             }
-            return next()
+
+            next()
+        } catch (err) {
+            res.status(500).send({ status: false, msg: err.message })
         }
-        if (req.params.blogId) {
-            let blogId = req.params.blogId
-            if (!ObjectID.isValid(blogId)) { return res.status(400).send({ status: false, message: "Not a valid BlogID" }) }
-            let check = await blogModel.findById(blogId)
-            if (!check) { return res.status(404).send({ status: false, message: "No such blog exists" }) }
-            if (check.authorId != decodedToken.authorId) {
-                return res.status(403).send({ status: false, message: "You are not a authorized user" })
+    }
+
+
+//*********************************************AUTHORIZATION************************************************************************
+
+const authorization = function (req, res, next) {
+        try {
+            let token = req.headers["x-api-key"];
+            let decodedToken = jwt.verify(token, "functionUp-plutonium-project-key")
+            let loggedInAuthorId = decodedToken.authorId                                           //req.decodedToken.authorId
+            let requestAuthorId = req.query.authorId
+            if (requestAuthorId != loggedInAuthorId) {
+                return res.status(403).send({ status: false, message: "no permission" })
             }
-            return next()
-        }
-        else {
-            return res.status(403).send({ status: false, message: "AuthorID or BlogID is mandatory" })
+            next()
+        } catch (err) {
+            res.status(500).send({ status: false, msg: err.message })
         }
     }
-    catch (error) {
-        res.status(500).send({ status: false, message: error.message})
-    }
-}
 
 
 
 
-module.exports.authorization=authorization
-
-module.exports.getAuthorDetails = getAuthorDetails;
+    module.exports.authorization = authorization
+    module.exports.authentication = authentication
